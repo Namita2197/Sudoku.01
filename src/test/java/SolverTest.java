eee40e0c32b9c24a2342ece9e161e9715af907db
Namita Raghuvanshi <raghuvanshinamita@gmail.com>
2019-11-19 20:35:18 -0700

    testing complete

--- src/test/java/SolverTest.java
+++ src/test/java/SolverTest.java
@@ -1,5 +1,37 @@
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class SolverTest {
+    @Test
+    public void solverConstructorTest() throws IOException {
+        Solver solver = new Solver("input\\Puzzle-4x4-0001.txt","output.txt");
+        String gridValues[][]=solver.getGridValues();
+        int backTrackingCount=0;
+        int onePossibleCount=0;
+        int humanKindCount=0;
+        long totalTimeTaken1=0;
+        long totalTimeTaken2=0;
+        BackTracking backTracking=new BackTracking(gridValues);
+        assertTrue(solver.validity(gridValues, gridValues[0].length));
+        long startTime = System.nanoTime();
+
+
+        long startTime3 = System.nanoTime();
+        backTracking.solve();
+        long endTime3 = System.nanoTime();
+        long totalTimeTaken3=endTime3-startTime3;
+        backTrackingCount=backTracking.getBackTrackingCount();
+        gridValues = backTracking.getBoard();
+        backTracking.consoleDisplay();
+        long endTime = System.nanoTime();
+        long totalTimeTaken=endTime-startTime;
+        File output = new File("output.txt");
+        solver.writeOutputFile(output,gridValues,onePossibleCount,humanKindCount,backTrackingCount,totalTimeTaken,totalTimeTaken1,totalTimeTaken2,totalTimeTaken3);
+
+    }
 
 }
